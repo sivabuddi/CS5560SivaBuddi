a0a8f1989aba35e05164a388d81d40282d73c7d8
Siva Kumar <sivakumar.umkc.fall2019@gmail.com>
2020-02-25 22:25:46 -0600

    ICP5_1.c

---
+++ ICP5/PRACTICE_INCLASS/ICP5_1c.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import NGram
+
+# creating spark session
+spark = SparkSession.builder .appName("Ngram Example").getOrCreate()
+
+#creating dataframe of input
+wordDataFrame = spark.createDataFrame([
+    (0, ["Hi", "I", "heard", "about", "Spark"]),
+    (1, ["I", "wish", "Java", "could", "use", "case", "classes"]),
+    (2, ["Logistic", "regression", "models", "are", "neat"])
+], ["id", "words"])
+
+#creating NGrams with n=2 (two words)
+ngram = NGram(n=3, inputCol="words", outputCol="ngrams")
+ngramDataFrame = ngram.transform(wordDataFrame)
+
+# displaying the results
+ngramDataFrame.select("Ngrams").show(truncate=False)
+
+#closing the spark session
+spark.stop()
+
